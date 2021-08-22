ffdb5026449abd8fbf78b1a07c5ed713fd134ec7
duyan <duyan@MacBook-Air-cua-Phan.local>
2021-08-22 16:52:10 +0700

    config babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { mapOrder } from './utilities/sort.js';
 
 const app = express();
 const hostname = 'localhost';
 const port = 8017;
 app.get('/', (req, res) => {
-  res.end('<h1>Hello World!</h1><hr>');
+  res.end('<h1>Hello Worldssa1212ss!</h1><hr>');
 });
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
