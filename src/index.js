b9018220c143df7342321730fb57bb612470f2ce
duyan <duyan@MacBook-Air-cua-Phan.local>
2021-08-22 13:39:57 +0700

    Init project Nodejs

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ app.get('/', (req, res) => {
   res.end('<h1>Hello World!</h1><hr>');
 });
 app.listen(port, hostname, () => {
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
+  // eslint-disable-next-line no-console
+  console.log(`Hello An, I am running at ${hostname}:${port}/`);
 });
