b5d8ef29d1bb6930fb3cd2dba5f6ec583aafb408
duyan <duyan@MacBook-Air-cua-Phan.local>
2021-08-23 12:53:09 +0700

    update

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ connectDB().catch(console.log);
 app.get('/', (req, res) => {
   res.end('<h1>Hello Worldssa1212ss!</h1><hr>');
 });
-app.listen(env.PORT, env.HOST, () => {
-  console.log(`Hello An, I am running at ${env.HOST}:${env.PORT}/`);
+app.listen(env.APP_PORT, env.HOST, () => {
+  console.log(`Hello An, I am running at ${env.APP_HOST}:${env.APP_PORT}/`);
 });
